7bb1a6f551001878231c2fae3932ee06e392bd26
Ime-android <44164134+Ime-android@users.noreply.github.com>
2020-01-01 15:23:26 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -24,7 +24,7 @@ var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
 if(x=="1"){xxx="/2018/10/couses-of-cancer-you-must-know.html"}
 if(x=="2"){xxx="/2018/10/ways-to-earn-money-with-bitcoin-7-ways.html"}
-if(x=="3"){xxx="/2018/10/get-money-from-forex-trading-you-sould.html"}a_to_ve[i].href="https://imeandroidsafelink.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="3"){xxx="/2018/10/get-money-from-forex-trading-you-sould.html"}a_to_ve[i].href="https://m.imeandroid.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
